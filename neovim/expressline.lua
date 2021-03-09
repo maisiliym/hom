d846fe6703561e740d03f2925ad721724377fc07
li <@li:maisiliym.uniks>
2021-03-09 14:54:11 +0000

    @try tj's statusline

--- neovim/expressline.lua
+++ neovim/expressline.lua
@@ -1,30 +1,70 @@
-local generator = function()
-  local builtin = require('el.builtin')
-  local extensions = require('el.extensions')
-  local sections = require('el.sections')
-  local subscribe = require('el.subscribe')
-  local helper = require('el.helper')
+local builtin = require('el.builtin')
+local extensions = require('el.extensions')
+local sections = require('el.sections')
+local subscribe = require('el.subscribe')
+local lsp_statusline = require('el.plugins.lsp_status')
+local helper = require('el.helper')
 
-  local segments = {}
+local has_lsp_extensions, ws_diagnostics =
+  pcall(require, 'lsp_extensions.workspace.diagnostic')
 
-  table.insert(segments,
-    subscribe.buf_autocmd("el_file_icon", "BufRead", function(_, buffer)
-      return extensions.file_icon(_, buffer)
-    end))
+local git_icon = subscribe.buf_autocmd("el_file_icon", "BufRead",
+  function(_, bufnr)
+    local icon = extensions.file_icon(_, bufnr)
+    if icon then return icon .. ' ' end
 
-  table.insert(segments, builtin.file)
+    return ''
+  end)
 
-  table.insert(segments, extensions.mode)
+local git_branch = subscribe.buf_autocmd("el_git_branch", "BufEnter",
+  function(window, buffer)
+    local branch = extensions.git_branch(window, buffer)
+    if branch then return ' ' .. extensions.git_icon() .. ' ' .. branch end
+  end)
 
-  local file_namer = function(_window, buffer) return buffer.name end
-  table.insert(segments, file_namer)
+local git_changes = subscribe.buf_autocmd("el_git_changes", "BufWritePost",
+  function(window, buffer) return extensions.git_changes(window, buffer) end)
 
-  table.insert(segments, extensions.git_changes)
+local ws_diagnostic_counts = function(_, buffer)
+  if not has_lsp_extensions then return '' end
 
-  table.insert(segments, helper.async_buf_setter(win_id, 'el_git_stat',
-    extensions.git_changes, 5000))
+  local messages = {}
 
-  return segments
+  local error_count = ws_diagnostics.get_count(buffer.bufnr, "Error")
+
+  local x = "⬤"
+  if error_count == 0 then
+    -- pass
+  elseif error_count < 5 then
+    table.insert(messages, string.format('%s#%s#%s%%*', '%',
+      "StatuslineError" .. error_count, x))
+  else
+    table.insert(messages,
+      string.format('%s#%s#%s%%*', '%', "StatuslineError5", x))
+  end
+
+  return table.concat(messages, "")
 end
 
-require('el').setup { generator = generator }
+local show_current_func = function(window, buffer)
+  if buffer.filetype == 'lua' then return '' end
+
+  return lsp_statusline.current_function(window, buffer)
+end
+
+require('el').setup {
+  generator = function(_, _)
+    return {
+      extensions.gen_mode { format_string = ' %s ' }, git_branch, ' ',
+      sections.split, git_icon,
+      sections.maximum_width(builtin.responsive_file(140, 90), 0.30),
+      sections.collapse_builtin { ' ', builtin.modified_flag }, sections.split,
+      show_current_func, lsp_statusline.server_progress, ws_diagnostic_counts,
+      git_changes, '[', builtin.line_with_width(3), ':',
+      builtin.column_with_width(2), ']',
+      sections.collapse_builtin {
+        '[', builtin.help_list, builtin.readonly_list, ']'
+      }, builtin.filetype
+    }
+  end
+}
